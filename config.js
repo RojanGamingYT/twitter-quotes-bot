e4e1823c0b994021baad5ea93546950e43050b35
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-05-17 06:56:48 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-    consumer_key:         'zP4lY5B7PhY2Jfvs9aQnobozK',
-    consumer_secret:      'xjDJqqutI7sBG7rQtcURtMKv3Yz05tWcIcbTUAlv25i3JhiYat',
-    access_token:         '871362953353715712-gEBGmD30nwCxIoKjPrxHw7B6ST8oNJa',
-    access_token_secret:  'gG4Oh9b3rIInZwiEarS56rraSfGWm3sn5StKUjg4vcqou'
+    consumer_key:         process.env.ck,
+    consumer_secret:      process.env.cs,
+    access_token:         process.env.at,
+    access_token_secret:  process.env.ts
   }
   
