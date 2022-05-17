927a7676311704271c36ac7b52f0f4987344587a
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-05-17 06:41:49 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-    consumer_key:         'API key',
-    consumer_secret:      'API secret key',
-    access_token:         'Access token',
-    access_token_secret:  'Access token secret'
+    consumer_key:         'zP4lY5B7PhY2Jfvs9aQnobozK',
+    consumer_secret:      'xjDJqqutI7sBG7rQtcURtMKv3Yz05tWcIcbTUAlv25i3JhiYat',
+    access_token:         '871362953353715712-gEBGmD30nwCxIoKjPrxHw7B6ST8oNJa',
+    access_token_secret:  'gG4Oh9b3rIInZwiEarS56rraSfGWm3sn5StKUjg4vcqou'
   }
   
